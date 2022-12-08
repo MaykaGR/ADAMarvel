7e7d65a73c7d7addd7f2933dad720fbc43970e54
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2022-12-09 00:05:10 +0100

    pij

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,66 +1,27 @@
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
 import java.io.File
-import java.io.PrintWriter
 import javax.xml.parsers.DocumentBuilderFactory
 
-
 fun main() {
-    var exit = false
-    val ficheroXML = File("resources${System.getProperty("file.separator")}personajesLol.xml")
-    if(ficheroXML.exists()){
-        var nodoPadre: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(ficheroXML)
-        var nombres = nodoPadre.getElementsByTagName("name")
-        while(exit == false){
-            println("¿Qué deseas hacer? -1. Buscar personaje -2. Salir")
-            var option = readln()
-            if(option=="1"){
-                var encontrado = false
-                while(encontrado==false){
-                println("Introduce el nombre del personaje: ")
-                val nombre = readln()
-                var i = 0
-                while(i<nombres.length&&encontrado == false){
-                    if(nombres.item(i).textContent==nombre){
-                        println("Personaje encontrado. Generando informe...")
-                        val ficheroEscritura = File("personajes${System.getProperty("file.separator")}${nombre}.txt")
-                        val pw = PrintWriter(ficheroEscritura, Charsets.UTF_8)
-                        var texto = ""
-                        var personaje = nodoPadre.getElementsByTagName(nombre)
-                        texto = personaje.item(0).textContent
-                        /*for(j in 0..etiquetas.length-1){
-                            println(etiquetas.item(j))
-                            val campo = etiquetas.item(j) as Element
-                            //println("${campo.tagName}${campo.textContent}")
-                            //if(campo.tagName=="name"){println(campo.textContent)}
-                            when(campo.tagName){
-                                "name" -> texto+="name: ${campo.textContent}"
-                                "title" -> texto+="title: ${campo.textContent}"
-                                "blurb" -> texto+="blurb: ${campo.textContent}"
-                                "tags" -> texto+="tags: ${campo.textContent}"
-                            }*/
-                        //}
-                        pw.write(texto)
-                        pw.close()
-                        encontrado = true
-                    }
-                    i+=1
-                }
-                    if(encontrado==false){
-                        println("Personaje no encontrado. Vuelve a intentarlo...")}
-                }
-            }
-            else if(option=="2"){
-                println("Hasta pronto!")
-               exit = true
-            }
-            else{
-                println("Comando inválido")
-            }
-        }}
-    else{
-        println("No se encuentra el fichero XML.")
+    val api = APIMarvel()
+    val busq = api.buscarPersonaje("3-D Man")
+    var personaje: Personaje? = null
+    if(busq.containsKey("200")){
+        personaje = busq.getValue("200")
+        println(personaje)
+        println(personaje?.comics?.size)
     }
+    else{
+        println("Personaje no encontrado")
+    }
+    val busq2 = api.buscarPersonaje("Agent Zero")
+    var personaje2: Personaje? = null
+    if(busq2.containsKey("200")){
+        personaje2 = busq2.getValue("200")
+        println(personaje2)
+        println(personaje2?.comics?.size)
+    }
+    else{
+        println("Personaje no encontrado")
+    }
+    println(api.popularidad())
 }
